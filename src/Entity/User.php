6dc270cf064226b3eff79b7e63d4d8a9045b67ef
C00MAR <moureau.marc78@gmail.com>
2024-07-16 16:24:30 +0200

    :tada: Update User Password & jwt connection

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,11 +6,36 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 use ApiPlatform\Metadata\ApiResource;
+use Symfony\Component\Serializer\Attribute\Groups;
+use Symfony\Component\Uid\Uuid;
+use Doctrine\ORM\Mapping\HasLifecycleCallbacks;
+use Doctrine\ORM\Mapping\PrePersist;
+
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Delete;
+use App\State\UserPasswordHasherProcessor;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
-#[ApiResource()]
+#[ApiResource(
+    normalizationContext: ['groups' => ['read']],
+    denormalizationContext: ['groups' => ['write']],
+    operations: [
+        new GetCollection(),
+        new Post(processor: UserPasswordHasherProcessor::class),
+        new Get(),
+        new Put(processor: UserPasswordHasherProcessor::class),
+        new Patch(processor: UserPasswordHasherProcessor::class),
+        new Delete(),
+    ],
+)]
 class User
 {
     #[ORM\Id]
@@ -22,7 +47,11 @@ class User
     private ?string $slug = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups('read')]
     private ?string $password = null;
+
+    #[Groups('write')]
+    private ?string $plainPassword = null;
 
     #[ORM\Column(length: 255)]
     private ?string $role = null;
@@ -63,6 +92,18 @@ class User
     public function setPassword(string $password): static
     {
         $this->password = $password;
+
+        return $this;
+    }
+
+    public function getPlainPassword(): string
+    {
+        return $this->plainPassword;
+    }
+
+    public function setPlainPassword(string $plainPassword): static
+    {
+        $this->plainPassword = $plainPassword;
 
         return $this;
     }
